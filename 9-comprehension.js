05d1ad7386b12eeb6703e75030f9efe5e7ce12d2
Sayanee <sayanee@gmail.com>
2013-05-06 09:02:29 +0800

    added code for array comprehension

--- 9-comprehension.js
+++ 9-comprehension.js
@@ -1,2 +1,10 @@
 // Understanding function scoped and variable hoisting
 // Run in : Firefox browser > Web Developer > Web Console
+
+function degToKelvin(deg) {
+  return deg + 273;
+}
+
+// 1D array is created
+var temperature = [0, 37, 100];
+[degToKelvin(t) for (t of temperature)]; // [273, 310, 373]
